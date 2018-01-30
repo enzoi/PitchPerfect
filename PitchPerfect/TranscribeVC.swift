c37b1de14008ea5b488f920a4b389db6c3afb714
Yeontae Kim <ytk007@gmail.com>
2018-01-30 00:25:53 -0800

    Adding transcribe related VC and UIs

---
+++ PitchPerfect/TranscribeVC.swift
@@ -0,0 +1,21 @@
+//
+//  TranscribeVC.swift
+//  PitchPerfect
+//
+//  Created by Yeontae Kim on 1/30/18.
+//  Copyright © 2018 YTK. All rights reserved.
+//
+
+import UIKit
+
+class TranscribeVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+
+}
